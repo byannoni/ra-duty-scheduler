534c1795a5f5c622d9715a6e4789b984a5d3587b
Brandon Yannoni <byannoni+github@gmail.com>
2018-05-03 18:49:57 -0400

    Attempt at a new scheduling system in chunks
    This attempts to schedule chunks of the calender randomly with RAs. Each
    chunk has random assignments but distribution is not that random (maybe
    for the better). It seems like it will be more fair. Does not account
    for remainder days yet.

--- Code.js
+++ Code.js
@@ -23,43 +23,75 @@ function is_weekend(date)
   return date.getDay() >= 4;
 }
 
-function do_ra_duty_generation(calendar_name, ras_list, start_date, end_date, break_start, break_end)
+function is_valid_date(date)
+{
+  Logger.log("Call to is_valid_date() with '" + date + "'");
+  return date && !isNaN(Date.parse(date));
+}
+
+function do_ra_duty_generation(calendar_name, ras_list, start_date_arg,
+    end_date_arg, break_start_arg, break_end_arg)
 {
   Logger.log("Querying calendars for '" + calendar_name + "'");
   var calendar_list = CalendarApp.getCalendarsByName(calendar_name);
   var ra_objects = ras_list.split('\n').map(create_ra_object);
   var cal = null;
 
-  var week_duties = 0;
-  var weekend_duties = 0;
+  var weekdays = [];
+  var weekends = [];
   var week_duties_per_ra = 0;
   var weekend_duties_per_ra = 0;
   var week_duties_remainder = 0;
   var weekend_duties_remainder = 0;
   var remaining_duties = 0;
   var days = [];
-  var break_dates_set = !(isNaN(break_start.getTime()) || isNaN(break_end.getTime()));
+  var break_dates_set;
 
-  start_date = new Date(start_date);
-  end_date = new Date(end_date);
-  break_start = new Date(break_start);
-  break_end = new Date(break_end);
+  var start_date = null;
+  var end_date = null;
+  var break_start = null;
+  var break_end = null;
 
   Logger.log("Validating dates");
 
-  if(isNaN(start_date.getTime()) || isNaN(end_date.getTime())) {
+  if(is_valid_date(break_start_arg)) {
+    break_start = new Date(break_start_arg);
+  }
+
+  if(is_valid_date(break_end_arg)) {
+    break_end = new Date(break_end_arg);
+  }
+
+  break_dates_set = is_valid_date(break_start_arg) && is_valid_date(break_end_arg);
+
+  if(!is_valid_date(start_date_arg)) {
     return JSON.stringify({
       status : false,
-      error : "One or more specified dates in invalid",
+      error : "The start date was invalid",
+    });
+  } else {
+    start_date = new Date(start_date_arg);
+  }
+
+  if(!is_valid_date(end_date_arg)) {
+    return JSON.stringify({
+      status : false,
+      error : "The end date was invalid",
+    });
+  } else {
+    end_date = new Date(end_date_arg);
+  }
+
+  Logger.log("Checking that the break was not only partially specified");
+
+  if(is_valid_date(break_start_arg) != is_valid_date(break_end_arg)) {
+    return JSON.stringify({
+      status : false,
+      error : "One of the break dates was specified but the other was in invalid",
     });
   }
 
-  if(isNaN(break_start.getTime()) != isNaN(break_end.getTime())) {
-    return JSON.stringify({
-      status : false,
-      error : "One of the break date was specified but the other was in invalid",
-    });
-  }
+  Logger.log("Checking that the semester start date is before the end date");
 
   if(start_date.getTime() > end_date.getTime()) {
     return JSON.stringify({
@@ -68,15 +100,10 @@ function do_ra_duty_generation(calendar_name, ras_list, start_date, end_date, br
     });
   }
 
-  if(break_start.getTime() > break_end.getTime()) {
-    return JSON.stringify({
-      status : false,
-      error : "The break start date is after the end date",
-    });
-  }
-
-  // Check that the break is during semester
   if(break_dates_set) {
+    // Check that the break is during semester
+    Logger.log("Checking that the break is during the semester");
+
     if(break_start.getTime() < start_date.getTime()
         || break_start.getTime() > end_date.getTime()
         || break_end.getTime() < start_date.getTime()
@@ -84,6 +111,15 @@ function do_ra_duty_generation(calendar_name, ras_list, start_date, end_date, br
       return JSON.stringify({
         status : false,
         error : "The break is not between the start and end dates",
+      });
+    }
+
+    Logger.log("Checking that the break starts before it ends");
+
+    if(break_start.getTime() > break_end.getTime()) {
+      return JSON.stringify({
+        status : false,
+        error : "The break start date is after the end date",
       });
     }
   }
@@ -106,86 +142,81 @@ function do_ra_duty_generation(calendar_name, ras_list, start_date, end_date, br
   for(var date_iter = new Date(start_date); date_iter <= end_date; date_iter.setDate(date_iter.getDate() + 1)) {
     // Skip days during the break
     if(break_dates_set && date_iter >= break_start && date_iter <= break_end) {
+      Logger.log("Day '" + date_iter + "' is during break");
       continue;
     }
 
+    Logger.log("Counting day '" + date_iter + "'");
+
     if(is_weekend(date_iter)) {
-      ++weekend_duties;
+      weekends.push(new Date(date_iter));
     } else {
-      ++week_duties;
+      weekdays.push(new Date(date_iter));
     }
 
-    days.push(date_iter);
+    days.push(new Date(date_iter));
   }
 
-  Logger.log((weekend_duties + week_duties) + " duties from " + start_date + " to " + end_date);
+  Logger.log((weekends.length + weekdays.length) + " duties from " + start_date + " to " + end_date);
+  Logger.log(days.length + " days in the array: " + JSON.stringify(days));
+
   // Calculate how many duties each RA gets and the remainder to unfairly assign
-  weekend_duties_per_ra = Math.floor((weekend_duties) / ra_objects.length);
-  weekend_duties_remainder = (weekend_duties) % ra_objects.length;
-  week_duties_per_ra = Math.floor((week_duties) / ra_objects.length)
-  week_duties_remainder = (week_duties) % ra_objects.length;
+  weekend_duties_per_ra = Math.floor(weekends.length * 2 / ra_objects.length);
+  weekend_duties_remainder = weekends.length * 2 % ra_objects.length;
+  week_duties_per_ra = Math.floor(weekdays.length * 2 / ra_objects.length)
+  week_duties_remainder = weekdays.length * 2 % ra_objects.length;
 
-  Logger.log("Weekend duties: " + weekend_duties + " (" + weekend_duties_per_ra + " per RA with " + weekend_duties_remainder + " remaining)");
-  Logger.log("Week duties: " + week_duties + " (" + week_duties_per_ra + " per RA with " + week_duties_remainder + " remaining)");  
+  Logger.log("Weekends: " + weekends.length + " (" + weekend_duties_per_ra + " per RA with " + weekend_duties_remainder + " remaining)");
+  Logger.log("Weekdays: " + weekdays.length + " (" + week_duties_per_ra + " per RA with " + week_duties_remainder + " remaining)");
   Logger.log("Assigning duties");
-  // Assign RAs for each day
-  for(var iter = 0; iter < days.length; ++iter) {
-    var primary_ra = null;
-    var secondary_ra = null;
 
-    Logger.log("Processing day " + days[iter]);
+	// Assign weekday duties
+	for(var segment = 0; segment < weekdays.length / ra_objects.length; ++segment) {
+		var available_primary_ras = ra_objects.slice();
+		var available_secondary_ras = ra_objects.slice();
 
-    do {
-      // Select a random RA for primary
-      primary_ra = ra_objects[Math.floor(Math.random() * ra_objects.length)];
+		while(weekdays.length > 0 && available_primary_ras.length > 0) {
+			var date = weekdays.shift();
+			var primary_ra = available_primary_ras.splice(Math.floor(Math.random() * available_primary_ras.length), 1)[0];
+			var valid_available_secondary_ras = available_secondary_ras.filter(function(ra) { return ra !== primary_ra; });
+			var secondary_ra = valid_available_secondary_ras[Math.floor(Math.random() * valid_available_secondary_ras.length)];
 
-      Logger.log("Considering RA '" + primary_ra.name + "' for primary on " + days[iter]);
+			Logger.log("Available secondary RAs: " + JSON.stringify(valid_available_secondary_ras, undefined, 2));
 
-      if(is_weekend(days[iter])) {
-        if(primary_ra.weekend_primary_duties < weekend_duties_per_ra || (primary_ra.weekend_primary_duties == weekend_duties_per_ra && weekend_duties_remainder >= weekend_duties)) {
-          ++primary_ra.weekend_primary_duties;
-          break;
-        }
-      } else {
-        if(primary_ra.week_primary_duties < week_duties_per_ra || (primary_ra.week_primary_duties == week_duties_per_ra && week_duties_remainder >= week_duties)) {
-          ++primary_ra.week_primary_duties;
-          break;
-        }
-      }
-    } while(true);
+			available_secondary_ras.splice(available_secondary_ras.indexOf(secondary_ra), 1);
 
-    Logger.log("Selected RA '" + primary_ra.name + "' for primary on " + days[iter]);
+			Logger.log("Selected RA '" + primary_ra.name + "' for primary on weekday " + date);
+			Logger.log("Selected RA '" + secondary_ra.name + "' for seconday on weekday " + date);
 
-    do {
-      // Select a random RA for secondary
-      secondary_ra = ra_objects[Math.floor(Math.random() * ra_objects.length)];
+			cal.createAllDayEvent(primary_ra.name + " / " + secondary_ra.name, date);
+			++primary_ra.week_primary_duties;
+			++secondary_ra.week_secondary_duties;
+		}
+	}
 
-      Logger.log("Considering RA '" + secondary_ra.name + "' for secondary on " + days[iter]);
+	// Assign weekend duties
+	for(var segment = 0; segment < weekends.length / ra_objects.length; ++segment) {
+		var available_primary_ras = ra_objects.slice();
+		var available_secondary_ras = ra_objects.slice();
 
-      if(secondary_ra != primary_ra) {
-        if(is_weekend(days[iter])) {
-          if(secondary_ra.weekend_secondary_duties < weekend_duties_per_ra || secondary_ra.weekend_secondary_duties == weekend_duties_per_ra && weekend_duties_remainder >= weekend_duties) {
-            ++secondary_ra.weekend_secondary_duties;
-            break;
-          }
-        } else {
-          if(secondary_ra.week_secondary_duties < week_duties_per_ra || secondary_ra.week_secondary_duties == week_duties_per_ra && week_duties_remainder >= week_duties) {
-            ++secondary_ra.week_secondary_duties;
-            break;
-          }
-        }
-      }
-    } while(true);
+		while(weekends.length > 0 && available_primary_ras.length > 0) {
+			var date = weekends.shift();
+			var primary_ra = available_primary_ras.splice(Math.floor(Math.random() * available_primary_ras.length), 1)[0];
+			var valid_available_secondary_ras = available_secondary_ras.filter(function(ra) { return ra !== primary_ra; });
+			var secondary_ra = valid_available_secondary_ras[Math.floor(Math.random() * available_secondary_ras.length)];
 
-    Logger.log("Selected RA '" + secondary_ra.name + "' for secondary on " + days[iter]);
+			Logger.log("Available secondary RAs: " + JSON.stringify(valid_available_secondary_ras, undefined, 2));
 
-    cal.createAllDayEvent(primary_ra.name + " / " + secondary_ra.name, days[iter]);
+			available_secondary_ras.splice(available_secondary_ras.indexOf(secondary_ra), 1);
 
-    if(is_weekend(days[iter]))
-      --weekend_duties;
-    else
-      --week_duties;
-  }
+			Logger.log("Selected RA '" + primary_ra.name + "' for primary on weekend " + date);
+			Logger.log("Selected RA '" + secondary_ra.name + "' for seconday on weekend " + date);
+
+			cal.createAllDayEvent(primary_ra.name + " / " + secondary_ra.name, date);
+			++primary_ra.weekend_primary_duties;
+			++secondary_ra.weekend_secondary_duties;
+		}
+	}
 
   Logger.log("Done");
 
@@ -198,3 +229,4 @@ function do_ra_duty_generation(calendar_name, ras_list, start_date, end_date, br
     ras : ra_objects,
   });
 }
+
